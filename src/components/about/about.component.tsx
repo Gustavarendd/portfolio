2114980797921b0de8a1a32c6431a1869b654204
Gustavarendd <111903425+Gustavarendd@users.noreply.github.com>
2023-04-16 13:38:11 +0200

    typo

--- src/components/about/about.component.tsx
+++ src/components/about/about.component.tsx
@@ -6,10 +6,10 @@ const About = () => {
       <AboutContainer>
         <h3>About Me</h3>
         <p>
-          I come from Denmark and is now living in Prague, Czech Republic. Since
-          2013 I have been working onboard sailing ships around the world. Most
-          recently I worked onboard a Danish school-ship, teaching the basic
-          education for trainees who want to start working onboard ships
+          I come from Denmark and I am now living in Prague, Czech Republic.
+          Since 2013 I have been working onboard sailing ships around the world.
+          Most recently I worked onboard a Danish school-ship, teaching the
+          basic education for trainees who want to start working onboard ships
           worldwide.
         </p>
       </AboutContainer>
